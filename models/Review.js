dac64a50d62e82a69500d08bd61e2ea9e4d169af
merahlwos-tech <merahlwos@gmail.com>
2026-05-08 12:14:54 +0200

    Update Review.js

--- models/Review.js
+++ models/Review.js
@@ -5,8 +5,8 @@ const reviewSchema = new mongoose.Schema(
     imageUrl:   { type: String, required: true },
     category:   {
       type:    String,
-      enum:    ['Board', 'Bags', 'Autocollants', 'Paper', 'general'],
-      default: 'general',
+      enum:    ['Board', 'Bags', 'Autocollants', 'Paper'],
+      default: 'Board',
     },
     githubPath: { type: String, default: '' }, // chemin dans le repo GitHub (pour suppression)
   },
